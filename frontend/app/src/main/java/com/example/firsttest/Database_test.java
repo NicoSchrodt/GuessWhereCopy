cace54f213fa12eb78e29b4bd2a994b942e1f7c4
NicoSchrodt <nico.schrodt@gmx.de>
2020-12-07 16:52:10 +0100

    Added functioning login and logout.

--- frontend/app/src/main/java/com/example/firsttest/Database_test.java
+++ frontend/app/src/main/java/com/example/firsttest/Database_test.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -28,31 +29,70 @@ public class Database_test extends AppCompatActivity {
         URL url = new URL("167.99.136.249/");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
          */
-        String urlParameters  = "param1=a&param2=b&param3=c";
-        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-        int    postDataLength = postData.length;
+        //String urlParameters  = "param1=a&param2=b&param3=c";
         String request        = "http://api.guesswhere.net/api.php?type=dbstatus";
         URL    url            = new URL( request );
         HttpURLConnection conn= (HttpURLConnection) url.openConnection();
-        conn.setDoOutput( true );
-        conn.setInstanceFollowRedirects( false );
+
+        //Request-Header
+        String urlParameters  = "";
+        byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
         conn.setRequestMethod( "POST" );
-        conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty( "charset", "utf-8");
-        conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+
+        //Send Post Request
         conn.setDoOutput(true);
-        conn.setUseCaches( false );
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        //conn.setInstanceFollowRedirects( false );
+        //conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty( "charset", "utf-8");
+        //conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+        //conn.setUseCaches( false );
+
+        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = conn.getResponseCode();
+        Log.d("Output","\nSending 'POST' request to URL : " + url);
+        Log.d("Output","Post parameters : " + urlParameters);
+        Log.d("Output","Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        Log.d("Output", response.toString());
+        //System.out.println(response.toString());
+
+
+        /*
+        OutputStream os = conn.getOutputStream();
+        os.write(urlParameters.getBytes("UTF-8"));
+        os.close();
+        Reader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
         try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
             wr.write( postData );
             String s = new String (postData, StandardCharsets.UTF_8);
             //String temp = bufferedReader.toString();
             String line = "";
-            while((line = bufferedReader.readLine()) != null){
-            }
+
+            //while((line = bufferedReader.readLine()) != null){
+            //}
+            //for (int c; (c = bufferedReader.read()) >= 0;)
+            //    Log.d("Output", Integer.toString(c));
             Log.d("Output", line);
             Log.d("Output", "test");
         }
+         */
+
 
     }
 }
